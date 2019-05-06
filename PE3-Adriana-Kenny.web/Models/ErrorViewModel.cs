46a7f26ea5ca3dfef7d30541e7fce9a85cbe02de
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2019-05-06 22:23:32 +0200

    Add project files.

---
+++ PE3-Adriana-Kenny.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PE3_Adriana_Kenny.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
