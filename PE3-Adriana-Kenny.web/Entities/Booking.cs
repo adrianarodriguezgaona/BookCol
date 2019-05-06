6ecd27bc749536dd9dba33734a84477d20e28964
Liro1984 <kenny.Zasada@student.howest.be>
2019-05-06 23:13:38 +0200

    Creation of Entities:  The entities Booking and Client are created with the first field annotations

---
+++ PE3-Adriana-Kenny.web/Entities/Booking.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PE3_Adriana_Kenny.web.Entities
+{
+    public class Booking
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long BookingId { get; set; }
+
+        [Required]
+        public DateTime CheckInDate { get; set; }
+
+        [Required]
+        public DateTime CheckOutDate { get; set; }
+
+        public long CustomerId { get; set; }
+
+        [Required]
+        [MaxLength(2)]
+        public int NmbrOfPeople { get; set; }
+
+        public long RoomId { get; set; }
+    }
+}
---
+++ PE3-Adriana-Kenny.web/Entities/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PE3_Adriana_Kenny.web.Entities
+{
+    public class Client
+    {   
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long KlantId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set;
+        }
+
+        [MaxLength(4)]
+        public int ZipCode { get; set; }
+
+        public string Community { get; set; }
+        public string Country { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string  Email { get; set; }
+    }
+}
