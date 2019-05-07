0b3b9dcb7771e54ee45cea661085b5c89428aee9
Liro1984 <kenny.Zasada@student.howest.be>
2019-05-07 08:44:43 +0200

    Context and seeding class created.

---
+++ PE3-Adriana-Kenny.web/Data/BookingContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PE3_Adriana_Kenny.web.Data
+{
+    public class BookingContext : DbContext
+    {
+        public BookingContext(DbContextOptions<BookingContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ PE3-Adriana-Kenny.web/Data/DataSeeder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PE3_Adriana_Kenny.web.Data
+{
+    public class DataSeeder
+    {
+    }
+}
