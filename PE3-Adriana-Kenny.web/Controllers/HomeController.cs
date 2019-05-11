fe4c44c036469c62b76231ae019338cf11418143
Liro1984 <kenny.Zasada@student.howest.be>
2019-05-11 16:24:30 +0200

    index page created with list of locations

--- PE3-Adriana-Kenny.web/Controllers/HomeController.cs
+++ PE3-Adriana-Kenny.web/Controllers/HomeController.cs
@@ -4,26 +4,30 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PE3_Adriana_Kenny.web.Data;
 using PE3_Adriana_Kenny.web.Models;
 
 namespace PE3_Adriana_Kenny.web.Controllers
 {
     public class HomeController : Controller
     {
+
+        private BookingContext bookingContext;
+
+        public HomeController(BookingContext context)
+        {
+            bookingContext = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var vm = new HomeIndexVM();
+
+            vm.Cities = bookingContext.Cities.OrderBy(c => c.Name).ToList();
+
+            return View(vm);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       
     }
 }
---
+++ PE3-Adriana-Kenny.web/Models/HomeIndexVM.cs
@@ -0,0 +1,13 @@
+﻿using PE3_Adriana_Kenny.web.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PE3_Adriana_Kenny.web.Models
+{
+    public class HomeIndexVM
+    {
+        public List<City> Cities { get; set; }
+    }
+}
