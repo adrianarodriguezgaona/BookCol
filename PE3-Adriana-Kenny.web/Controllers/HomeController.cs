b6c8d1cf22c15d3baf7cf431d5a085a2e1451d24
Liro1984 <kenny.Zasada@student.howest.be>
2019-05-13 16:39:53 +0200

    Creation of the login form + Server & client side validation added.  Posting to the client table in database now possible

--- PE3-Adriana-Kenny.web/Controllers/HomeController.cs
+++ PE3-Adriana-Kenny.web/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PE3_Adriana_Kenny.web.Data;
+using PE3_Adriana_Kenny.web.Entities;
 using PE3_Adriana_Kenny.web.Models;
 
 namespace PE3_Adriana_Kenny.web.Controllers
@@ -28,6 +29,41 @@ namespace PE3_Adriana_Kenny.web.Controllers
             return View(vm);
         }
 
-       
+
+        public IActionResult RegisterForm()
+
+        {
+
+            var vm = new Client();
+
+
+            return View(vm);
+
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult RegisterForm(Client client)
+        {
+
+            if (ModelState.IsValid)
+
+            {
+
+
+                bookingContext.Clients.Add(client);
+                bookingContext.SaveChanges();
+
+
+
+                return RedirectToAction("Index");
+            }
+
+            else return View(client);
+
+        }
+
+
     }
 }
