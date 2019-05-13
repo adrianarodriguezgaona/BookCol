07cdfc24d578ada6b2ce473424bd28fec33f0e51
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2019-05-13 11:36:31 +0200

    Add viewcomponent
    To use in BookcolLayout to have a dropdown  of cities with links

---
+++ PE3-Adriana-Kenny.web/Components/NavFooter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PE3_Adriana_Kenny.web.Data;
+using PE3_Adriana_Kenny.web.Models;
+
+namespace PE3_Adriana_Kenny.web.Components
+{
+    [ViewComponent(Name = "NavFooter")]
+    public class NavFooterComponent : ViewComponent
+    {
+        private BookingContext bookingContext;
+
+        public NavFooterComponent(BookingContext context)
+        {
+            bookingContext = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var vm = new HomeIndexVM();
+
+            vm.Cities = bookingContext.Cities.OrderBy(c => c.Name).ToList();
+
+            return View(vm);
+        }
+    }
+}
+
