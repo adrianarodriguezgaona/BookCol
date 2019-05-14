191e3944dfcf6c1a1431374029940d392fb1f69f
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2019-05-14 22:19:45 +0200

    Add migration
    To update the database becouse of changes in hotel propertie photo

---
+++ PE3-Adriana-Kenny.web/Migrations/20190514170612_UpdateHotelsPhoto.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PE3_Adriana_Kenny.web.Migrations
+{
+    public partial class UpdateHotelsPhoto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Clients",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "Photo",
+                value: "FourSeasonsCasaMedina.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 5L,
+                column: "Photo",
+                value: "ExpoSuitesParque.jpg");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Clients",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "Photo",
+                value: "FourSeasonsCasaMedina");
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 5L,
+                column: "Photo",
+                value: "ExpoSuitesParque");
+        }
+    }
+}
