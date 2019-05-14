87f3cb6171e59f3e879f38f195ceb95d1c612bf5
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2019-05-14 22:14:46 +0200

    Add 2 Actions
    Now we have a view with all hotels and another with hotels by city id.

--- PE3-Adriana-Kenny.web/Controllers/HotelController.cs
+++ PE3-Adriana-Kenny.web/Controllers/HotelController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PE3_Adriana_Kenny.web.Data;
+using PE3_Adriana_Kenny.web.Models;
 
 namespace PE3_Adriana_Kenny.web.Controllers
 {
     public class HotelController : Controller
     {
+        private BookingContext bookingContext;
+
+        public HotelController (BookingContext context)
+        {
+            bookingContext = context;
+        }
+
 
         public IActionResult FilterHotel()
         {
@@ -16,6 +26,27 @@ namespace PE3_Adriana_Kenny.web.Controllers
             return View();
         }
 
+        public IActionResult HotelDiv ( )
+        {
+            var vm = new HotelFilterHotelVM();
 
+            vm.Hotels = bookingContext.Hotels
+                       .Include(c => c.City)
+                       .ToList();
+
+            return View(vm);
+        }
+
+        public IActionResult HotelByCity(long id)
+        {
+            var vm = new HotelFilterHotelVM();
+
+            vm.Hotels = bookingContext.Hotels
+                       .Include(c => c.City)
+                       .Where(h => h.CityId == id)
+                       .ToList();
+
+            return View(vm);
+        }
     }
 }
