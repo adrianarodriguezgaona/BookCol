47013f714b884042780ddf13b0edf78a7beeea95
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2019-05-17 10:47:47 +0200

    migra

--- PE3-Adriana-Kenny.web/Migrations/20190517084719_Newproperties-Hotel-Roomtype.Designer.cs
+++ PE3-Adriana-Kenny.web/Migrations/20190517084719_Newproperties-Hotel-Roomtype.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PE3_Adriana_Kenny.web.Data;
 
 namespace PE3_Adriana_Kenny.web.Migrations
 {
     [DbContext(typeof(BookingContext))]
-    partial class BookingContextModelSnapshot : ModelSnapshot
+    [Migration("20190517084719_Newproperties-Hotel-Roomtype")]
+    partial class NewpropertiesHotelRoomtype
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
