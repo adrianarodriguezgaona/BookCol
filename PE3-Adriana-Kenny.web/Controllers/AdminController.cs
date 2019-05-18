8b8d8109f7f8e0e6214844bcd0a1d44411144e13
Liro1984 <kenny.Zasada@student.howest.be>
2019-05-18 16:46:38 +0200

    created new Admin Controller: Linked to admin page, where a hotel can be created or Edited/deleted.  Edit delete works, creation still has to be implemented

--- PE3-Adriana-Kenny.web/Controllers/AdminController.cs
+++ PE3-Adriana-Kenny.web/Controllers/AdminController.cs
@@ -3,14 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PE3_Adriana_Kenny.web.Data;
+using PE3_Adriana_Kenny.web.Entities;
+using PE3_Adriana_Kenny.web.Models;
 
 namespace PE3_Adriana_Kenny.web.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
+
+
+        private BookingContext bookingContext;
+
+        public AdminController(BookingContext context)
         {
-            return View();
+            bookingContext = context;
         }
+
+        public IActionResult Index()
+
+      
+        {
+            var vm = new AdminIndexVM();
+                vm.Hotels = bookingContext.Hotels.OrderBy(h => h.Name).ToList();
+
+                return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Index(long id)
+
+        {
+
+            return RedirectToAction("AdminEditRemove", new { id = id });
+        }
+
+        public IActionResult AdminEditRemove(long id)
+
+        {
+            var vm = new Hotel();
+
+                vm = bookingContext.Hotels.Find(id);
+
+            return View(vm);
+        }
+
+
+        [HttpPost]
+        public IActionResult HotelEdit(Hotel hotel, string submitbutton)
+
+        {
+            if (submitbutton == "Wijzigen")
+            {
+                bookingContext.Update(hotel);
+                bookingContext.SaveChanges();
+
+          
+            }
+            else { bookingContext.Remove(hotel);
+                bookingContext.SaveChanges();
+
+               
+            }
+            return Redirect("index");
+        }
+
+
+        
+        
+        
+
+
+
     }
 }
