ba8dc76aa66cda9a7d16d3a2deeb94e2a21f5baa
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2019-05-28 17:42:01 +0200

    Created Viecomponent for the login user indicators

--- PE3-Adriana-Kenny.web/Models/UserState.cs
+++ PE3-Adriana-Kenny.web/Models/UserState.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PE3_Adriana_Kenny.web.Entities;
 
 namespace PE3_Adriana_Kenny.web.Models
 {
@@ -9,5 +10,6 @@ namespace PE3_Adriana_Kenny.web.Models
     {
         public bool  IsLoggedIn { get; set; }
         public bool  IsAdmin { get; set; }
+        public Client Client { get; set; }
     }
 }
