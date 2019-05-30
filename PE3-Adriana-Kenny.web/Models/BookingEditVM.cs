41471415d6ab645a935e5e3310b48c47fd2dd3f0
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2019-05-30 09:58:15 +0200

    this models are maked to use by the controller to send to the view a Ienumerable of booking and a specific booking to edit

---
+++ PE3-Adriana-Kenny.web/Models/BookingEditVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PE3_Adriana_Kenny.web.Entities;
+
+namespace PE3_Adriana_Kenny.web.Models
+{
+    public class BookingEditVM : BookingCreateVM
+    {
+        public long Id { get; set; }
+        public List<Client> Clients { get; set; }
+    }
+}
---
+++ PE3-Adriana-Kenny.web/Models/BookingIndexVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PE3_Adriana_Kenny.web.Entities;
+
+namespace PE3_Adriana_Kenny.web.Models
+{
+    public class BookingIndexVM
+    {
+        public IEnumerable<Booking>Bookings { get; set; }
+    }
+}
