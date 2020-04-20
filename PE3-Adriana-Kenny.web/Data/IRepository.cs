7167af7fb1c955f2d65890166583f22bad7257dd
Adriana Rodriguez <adriana@codekick.be>
2020-04-20 12:54:45 +0200

    Interface made for working with dependency injection

---
+++ PE3-Adriana-Kenny.web/Data/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PE3_Adriana_Kenny.web.Entities;
+
+namespace PE3_Adriana_Kenny.web.Data
+{
+    public interface IRepository <T, Tkey> where T : BaseEntity <Tkey>
+    {
+        Task<T> GetByIdAsync(Tkey id);
+        IQueryable<T> GetAll();
+        Task<T> AddAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task UpdateAsync(T entity);
+    }
+}
